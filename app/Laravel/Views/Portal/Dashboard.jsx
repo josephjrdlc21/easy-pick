ee7fc2b682d27171e90ba0173b5e686aece8b423
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-08-04 23:17:08 +0800

    clean up library

--- app/Laravel/Views/Portal/Dashboard.jsx
+++ app/Laravel/Views/Portal/Dashboard.jsx
@@ -1,9 +1,11 @@
 import Main from "./_layouts/main";
+import { Button } from 'react-bootstrap';
 
 export default function Dashboard() {
     return(
         <Main>
-            <h1>Hello World</h1>
+            <Button variant="primary">Primary</Button>
+            
         </Main>
     );
 }
--- app/Laravel/Views/Portal/_layouts/main.jsx
+++ app/Laravel/Views/Portal/_layouts/main.jsx
@@ -1,5 +1,10 @@
 import Sidebar from "../_components/SideBar";
 
+import '../_assets/modules/bootstrap/css/bootstrap.min.css';
+import '../_assets/modules/fontawesome/fontawesome/all.min.css';
+import '../_assets/css/style.css';
+import '../_assets/css/components.css';
+
 export default function Main({ children }) {
     return(
        <>
