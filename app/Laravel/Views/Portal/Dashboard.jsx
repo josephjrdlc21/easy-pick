20f3269c9c0da0068808bfa014a214a2d837225e
Joseph Dela Cruz Jr <josephdelacruzjr4@gmail.com>
2025-08-04 13:49:30 +0800

    remove template

--- app/Laravel/Views/Portal/Dashboard.jsx
+++ app/Laravel/Views/Portal/Dashboard.jsx
@@ -1,11 +1,8 @@
-import MainLayout from "./_layouts/MainLayout";
 
 export default function Dashboard() {
     return(
         <>
-            <MainLayout>
-                <h1>asdasdas</h1>
-            </MainLayout>
+          <h1>Hello Word</h1>
         </>
     );
 }
